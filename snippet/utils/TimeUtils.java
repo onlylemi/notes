3b03d23273c871e3bb7216932f4c4a57ae652506
onlylemi <xiaomi_0623@163.com>
2016-07-09 22:20:41 +0800

    TimeUtils 工具类

---
+++ snippet/utils/TimeUtils.java
@@ -0,0 +1,58 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * TimeUtils
+ *
+ * @author: onlylemi
+ */
+public class TimeUtils {
+
+    private TimeUtils() {
+    }
+
+    /**
+     * 获取当前时间戳
+     *
+     * @return
+     */
+    public static int currentTimestamp() {
+        return (int) (System.currentTimeMillis() / 1000);
+    }
+
+    /**
+     * 时间戳转日期
+     *
+     * @param timestamp
+     * @param format
+     * @return
+     */
+    public static String timestampToDate(long timestamp, String format) {
+        if (null == format || format.isEmpty()) {
+            format = "yyyy-MM-dd HH:mm:ss";
+        }
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
+        return sdf.format(new Date(timestamp * 1000));
+    }
+
+    /**
+     * 日期转时间戳
+     *
+     * @param date
+     * @param format
+     * @return
+     */
+    public static long dateToTimestamp(String date, String format) {
+        if (null == format || format.isEmpty()) {
+            format = "yyyy-MM-dd HH:mm:ss";
+        }
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
+        try {
+            return sdf.parse(date).getTime() / 1000;
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+}
