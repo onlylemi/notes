e0df430a8f90d775caf3e347be24b9635ecc309c
onlylemi <xiaomi_0623@163.com>
2016-07-09 22:13:11 +0800

    Application

---
+++ snippet/App.java
@@ -0,0 +1,22 @@
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * App
+ *
+ * @author: onlylemi
+ */
+public class App extends Application {
+
+    private static Context context;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context = getApplicationContext();
+    }
+
+    public static Context getContext() {
+        return context;
+    }
+}
