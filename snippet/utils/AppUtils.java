96030afd698c157b0620c759f40fc7f105b2dc99
onlylemi <xiaomi_0623@163.com>
2016-07-22 21:42:30 +0800

    AppUtils

--- snippet/utils/AppUtils.java
+++ snippet/utils/AppUtils.java
@@ -100,4 +100,28 @@ public class AppUtils {
         }
         return 1;
     }
+
+    /**
+     * 屏幕的width
+     *
+     * @param activity
+     * @return
+     */
+    public static int screeWidth(Activity activity) {
+        DisplayMetrics metrics = new DisplayMetrics();
+        activity.getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        return metrics.widthPixels;
+    }
+
+    /**
+     * 屏幕的高
+     *
+     * @param activity
+     * @return
+     */
+    public static int screeHeight(Activity activity) {
+        DisplayMetrics metrics = new DisplayMetrics();
+        activity.getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        return metrics.heightPixels;
+    }
 }
